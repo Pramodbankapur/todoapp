38ea427369ed731c002a79c35171a4e22e9529c2
Undefined <undefined@Undefined.local>
2026-02-01 18:34:24 +0530

    Recovred all files from command

--- todo-app/frontend/src/App.tsx
+++ todo-app/frontend/src/App.tsx
@@ -1,16 +1,10 @@
-import { useEffect } from "react";
 import { SignupForm } from "./AuthPage/Pages/SignupForm/auth/SignupForm";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { LoginForm } from "./AuthPage/Pages/LoginForm/components/LoginForm";
 
 
 function App() {
-  // useEffect(()=>{
-  //   fetch('http://localhost:5000/api/health')
-  //   .then(res=>res.json())
-  //   .then(data=>console.log('Backend says:',data))
-  //   .catch(err=>console.error(err));
-  // },[])
+
 
   return (
     <BrowserRouter>
